89e0fc869000d462e54a2486bc6919f7c0b71a61
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-10-14 01:13:24 -0400

    fixed the tailwindcss file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+  ],
   theme: {
     extend: {},
   },
